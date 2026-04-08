b18bf07c84044e5baefd67fef255a34f588974e2
Gareth Wright <garpunkal@gmail.com>
2026-04-08 14:51:27 +0100

    refactor: move client scripts to entry file for better organization and modularity

---
+++ src/scripts/client/entry.js
@@ -0,0 +1,7 @@
+import { initAnalytics } from "./analytics.ts";
+import { initThemeToggle } from "./theme-toggle.ts";
+import { initSmoothScroll } from "./smooth-scroll.ts";
+
+initAnalytics();
+initThemeToggle();
+initSmoothScroll();
